81bc19b58f6023206b4bbc9da884d7a201b108fe
dhruvp-8 <dhruvpatel5738@gmail.com>
2020-05-14 04:33:49 -0700

    Added Average Response Time Query #4

--- src/analytics.go
+++ src/analytics.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
@@ -31,6 +32,39 @@ func (m mongo) Write(r requestAnalytics) error {
 
 func (m mongo) Count() (int, error) {
 	return m.sess.DB("metrics_db").C(collectionName).Count()
+}
+
+type statsPerRoute struct {
+	ID struct {
+		Method string `bson:"method" json:"method"`
+		URL    string `bson:"url" json:"url"`
+	} `bson:"_id" json:"id"`
+	NumberOfRequests int `bson:"numberOfRequests" json:"number_of_requests"`
+}
+
+func (m mongo) AverageResponseTime() (float64, error) {
+
+	type res struct {
+		AverageResponseTime float64 `bson:"averageResponseTime" json:"average_response_time"`
+	}
+
+	var ret = []res{}
+
+	var baseMatch = bson.M{
+		"$group": bson.M{
+			"_id":                 nil,
+			"averageResponseTime": bson.M{"$avg": "$requesttime"},
+		},
+	}
+
+	err := m.sess.DB("metrics_db").C(collectionName).
+		Pipe([]bson.M{baseMatch}).All(&ret)
+
+	if len(ret) > 0 {
+		return ret[0].AverageResponseTime, err
+	}
+
+	return 0, nil
 }
 
 func newMongo(addr string) (mongo, error) {
