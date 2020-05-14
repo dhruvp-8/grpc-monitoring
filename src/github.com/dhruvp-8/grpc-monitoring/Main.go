23f9ca5e931192b23017486fa68a5baa8d6ec9e5
dhruvp-8 <dhruvpatel5738@gmail.com>
2020-05-13 23:32:06 -0700

    Added Pusher env variables and Mongo Cxn

--- src/github.com/dhruvp-8/grpc-monitoring/Main.go
+++ src/github.com/dhruvp-8/grpc-monitoring/Main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	// Function Stub
+	httpPort := flag.Int("http.port", 4000, "HTTP Port to run server on")
+	mongoDSN := flag.String("mongo.dsn", "localhost:27017", "DSN for mongoDB server")
+
+	flag.Parse()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	appID := os.Getenv("PUSHER_APP_ID")
+	appKey := os.Getenv("PUSHER_APP_KEY")
+	appSecret := os.Getenv("PUSHER_APP_SECRET")
+	appCluster := os.Getenv("PUSHER_APP_CLUSTER")
+	appIsSecure := os.Getenv("PUSHER_APP_SECURE")
 }
