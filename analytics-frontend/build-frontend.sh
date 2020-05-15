2686a71dc6403538176bc5655e75b4497e09e1b7
dhruvp-8 <dhruvpatel5738@gmail.com>
2020-05-14 18:49:20 -0700

    Added the bash file

--- analytics-frontend/build-frontend.sh
+++ analytics-frontend/build-frontend.sh
@@ -3,5 +3,6 @@
 echo "creating the build file for the project"
 yarn build
 echo "moving the build file to ../src/static"
-mv build ../src/static
+rm -rf ../src/public/build
+mv build ../src/public
 
