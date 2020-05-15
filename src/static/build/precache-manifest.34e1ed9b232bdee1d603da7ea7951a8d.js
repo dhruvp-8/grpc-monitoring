82df05ad1540926cb8266bc3f96d93e9dab35067
santhosh96 <santhosn@usc.edu>
2020-05-14 17:36:42 -0700

    Initial Commit for React Frontend. Fix #2

---
+++ src/static/build/precache-manifest.34e1ed9b232bdee1d603da7ea7951a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0e0b97c5853f656894c2b0584f5a37",
+    "url": "../src/static/index.html"
+  },
+  {
+    "revision": "f222a27a5083fe962bd5",
+    "url": "../src/static/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eaf6c07387e600afc158",
+    "url": "../src/static/static/js/2.03007d1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "../src/static/static/js/2.03007d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f222a27a5083fe962bd5",
+    "url": "../src/static/static/js/main.005b8e09.chunk.js"
+  },
+  {
+    "revision": "153fdabb64638da2f646",
+    "url": "../src/static/static/js/runtime-main.784e330a.js"
+  }
+]);
