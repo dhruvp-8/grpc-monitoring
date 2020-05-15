8497e95bf9b8bb481697349929bec11b3131b149
Santhosh Narayanan <santhosn@usc.edu>
2020-05-14 23:51:04 -0700

    raw data displayed for average response time, total requests and individual route request

--- analytics-frontend/src/components/ApplicationDashboard.js
+++ analytics-frontend/src/components/ApplicationDashboard.js
@@ -3,17 +3,36 @@ import axios from 'axios';
 
 class ApplicationDashboard extends Component {
 	
+	state = {
+		avgRespTime : null,
+		totalRequests : null,
+		childStatus : []
+	}
+
 	componentDidMount() {
 		axios.get('http://localhost:4000/api/analytics')
 			.then(res => {
-				console.log(res["data"])
+
+				const responseData = res["data"]
+				console.log(responseData)
+				this.setState({
+					avgRespTime : responseData["average_response_time"],
+					childStatus : responseData["stats_per_route"],
+					totalRequests : responseData["total_requests"]
+				})
 			})
 	}
 	
 	render() {
 		return (
 			<div>
-				<h1>Count : 1</h1>
+				<h1>Total Requests : {this.state.totalRequests}</h1>
+				<h2>Average response time : {this.state.avgRespTime}</h2>
+				<ul>
+					{this.state.childStatus.map( stats => 
+						<li key={stats.id.url}><b>URL:</b> {stats.id.url} <b>Number of Requests:</b> {stats.number_of_requests}</li>
+					)}
+				</ul>
 			</div>
 		)
 	}
